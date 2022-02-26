1bbfc185b2e872c1ce3ea5bb9af17a6686042d35
Ashley's PC <ashlynn4567@gmail.com>
2022-02-26 15:50:37 -0600

    added password specification prompts and prompt validation for types

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,121 +1,165 @@
-// ---------------------------------------------------------------------------------------- //
-// TABLE OF CONTENTS: --------------------------------------------------------------------- //
-// --------------------------------------------------------------------------1. lorem ipsum //
-// --------------------------------------------------------------------------2. lorem ipsum //
-// --------------------------------------------------------------------------3. lorem ipsum //
-// ---------------------------------------------------------------------------------------- //
+// --------------------------------------------------------------------------------------------------------------------- //
+// TABLE OF CONTENTS: -------------------------------------------------------------------------------------------------- //
+// ---------------------------------------------------------------------------------------------------------1. VARIABLES //
+// ----------------------------------------------------------2. GETTING INPUT FROM THE USER AND STORING IT AS A VARIABLE //
+// -----------------------------------------------------3. RANDOMLY GENERATING A PASSWORD CHARACTERS BASED ON USER INPUT //
+// ------------------------------------------------------------------------4. WRITING PASSWORD ON THE PAGE FOR USER VIEW //
+// -----------------------------------------------------------------5. CLICK EVENT LISTENER TO SET THE PROGRAM IN MOTION //
+// --------------------------------------------------------------------------------------------------------------------- //
 
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// 1. VARIABLES--------------------------------------------------------------------------------------------------------- //
+// Nested array for possible password characters
+var characterSet = [  
+  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+  ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+  ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
+  [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
+];
+// END VARIABLE SECTION------------------------------------------------------------------------------------------------- //
 
+
+
+
+// 2. GETTING USER INPUT AND STORING IT AS A VARIABLE------------------------------------------------------------------- //
+// defining passwordSpecs object to hold user input values
+var passwordSpecs = {};
+
+function errorMessage(value, prompt) {
+  while (value === "" || value === null) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    value = window.prompt(prompt);
+  };
+};
+
+// function asking user to input different types of character specifications
+function getPasswordType() {
+  // set a variable to track if the user has entered yes to at least one character type
+  var oneType = false;
+
+  while (oneType === false) {
+    // lowercase? (yes or no)
+    var lowerCaseLettersPrompt = "Would you like to include lowercase letters? Type Y for YES or N for NO.";
+    passwordSpecs.lower = prompt(lowerCaseLettersPrompt);
+    console.log(passwordSpecs.lower);
+
+    if (passwordSpecs.lower == "Y" || passwordSpecs.lower == "y") {
+      oneType = true;
+    };
+   
+    errorMessage(passwordSpecs.lower, lowerCaseLettersPrompt);
+
+    // uppercase? (yes or no)
+    var upperCaseLettersPrompt = "Would you like to include uppercase letters? Type Y for YES or N for NO.";
+    passwordSpecs.upper = prompt(upperCaseLettersPrompt);
+    console.log(passwordSpecs.upper);
+
+    if (passwordSpecs.upper == "y" || passwordSpecs.upper == "y") {
+      oneType = true;
+    };
+    
+    errorMessage(passwordSpecs.upper, upperCaseLettersPrompt);
+
+    // numbers? (yes or no)
+    var numbersPrompt = "Would you like to include numerical characters? Type Y for YES or N for NO.";
+    passwordSpecs.number = prompt(numbersPrompt);
+    console.log(passwordSpecs.number);
+
+    if (passwordSpecs.number == "y" || passwordSpecs.number == "y") {
+      oneType = true;
+    };
+    
+    errorMessage(passwordSpecs.number, numbersPrompt);
+
+    // special characters? (yes or no)
+    var specialPrompt = "Would you like to include special characters? Type Y for YES or N for NO.";
+    passwordSpecs.special = prompt(specialPrompt);
+    console.log(passwordSpecs.special);
+
+    if (passwordSpecs.special == "y" || passwordSpecs.special == "y") {
+      oneType = true;
+    };
+    
+    errorMessage(passwordSpecs.special, specialPrompt);
+
+    // alert user to enter at least one Y value if they haven't already done so
+    if (oneType === false) {
+      window.alert("You must enter Y for YES to at least one character type.");
+    };
+  };
+};
+
+// function asking user to input number of characters 
+function getPasswordNumChars() {
+  // how many characters? (8 to 128)
+  passwordSpecs.numChars = prompt("How many characters do you need in your password? Enter a number from 8 to 128.");
+  passwordSpecs.numChars = parseInt(passwordSpecs.numChars);
+  console.log(passwordSpecs.numChars);
+
+  while (passwordSpecs.numChars === "" || passwordSpecs.numChars === null) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    passwordSpecs.numChars = prompt("How many characters do you need in your password? Enter a number from 8 to 128.");
+  };
+};
+
+// run all password specification functions
+function getPasswordSpecs() {
+  getPasswordType();
+  getPasswordNumChars();
+  console.log(passwordSpecs);
+};
+// END USER INPUT SECTION----------------------------------------------------------------------------------------------- //
+
+
+
+
+// 3. RANDOMLY GENERATING A PASSWORD CHARACTERS BASED ON USER INPUT----------------------------------------------------- //
+// use math.random to generate password
+function generatePassword() {
+  
+};
+// END PASSWORD GENERATION SECTION-------------------------------------------------------------------------------------- //
+
+
+
+
+// 4. WRITING PASSWORD ON THE PAGE FOR USER VIEW------------------------------------------------------------------------ //
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 };
+// END WRITE PASSWORD TO PAGE SECTION----------------------------------------------------------------------------------- //
 
-// gathering user input to generate a password
-function getPasswordSpecs() {
-  // defining passwordSpecs object
-  var passwordSpecs = {};
 
-  // how many characters? (8 to 128)
-  passwordSpecs.numChars = prompt("How many characters do you need in your password? Enter a number from 8 to 128.");
-  passwordSpecs.numChars = parseInt(passwordSpecs.numChars);
 
-  // lowercase? (yes or no)
-  passwordSpecs.lower = prompt("Would you like to include lowercase letters? Type Y for YES or N for NO.");
 
-  // uppercase? (yes or no)
-  passwordSpecs.upper = prompt("Would you like to include uppercase letters? Type Y for YES or N for NO.");
+// 5. CLICK EVENT LISTENER TO SET THE PROGRAM IN MOTION----------------------------------------------------------------- //
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
 
-  // numbers? (yes or no)
-  passwordSpecs.number = prompt("Would you like to include numerical characters? Type Y for YES or N for NO.");
-
-  // special characters? (yes or no)
-  passwordSpecs.special = prompt("Would you like to include special characters? Type Y for YES or N for NO.");
-};
-
-// use math.random to generate password
-function generatePassword() {
-  
-};
-  
 // Add event listener to generate button
 generateBtn.addEventListener("click", getPasswordSpecs);
+// END CLICK EVENT LISTENER SECTION------------------------------------------------------------------------------------- //
 
 
 
 
 
 
+/* QUESTIONS:
 
+1. How do I determine a good logical order for my layout of functions throughout my codebase?
+    a: scoping. but i don't understand this quite yet. 
+2. I made separate functions in passwordSpecs: 
+      - one for getPasswordNumChars 
+      - and another for getPasswordType. 
+    I did this because the functions needed to be separated for form validation reasons (i think).
+    Is this bad because they are nested in each other? Logically it makes sense to me but I don't 
+    want the codebase to be difficult to read/edit later. 
+3. Can you explain the difference between return and break? Once I set up form validation with a 
+    conditional, I don't understand how to make the code repeat like I want it to. 
 
-
-
-
-
-
-
-
-
-
-
-
-//     // entry validation
-//       // if (passwordLength === "" || passwordLength === null) {
-//       //   window.alert("You need to provide a valid answer! Please try again.");
-//       //   return generatePassword();
-//       // };
-
-//     // password length (8 - 128 characters)
-//     var getPasswordSpecs = function() {
-//       window.prompt("How long do you want your password to be? Please enter a length of at least 8 characters and no more than 128 characters.");
-//       console.log(getPasswordLength);
-//       if (getPasswordSpecs === "y" || getPasswordSpecs === "Y") {
-
-//       };
-//     };
-    
-//     // // lowercase? (yes or no)
-//     // var getPasswordLower = window.prompt("Would you like to include lowercase letters? Type Y for YES and N for NO.");
-//     // console.log(getPasswordLower);
-
-//     // // uppercase? (yes or no)
-//     // var getPasswordUpper = window.prompt("Would you like to include uppercase letters? Type Y for YES and N for NO.");
-//     // console.log(getPasswordUpper);
-
-//     // // numbers? (yes or no)
-//     // var getPasswordNumber = window.prompt("Would you like to include numeric characters? Type Y for YES and N for NO.");
-//     // console.log(getPasswordNumber);
-
-//     // // special characters? (yes or no)
-//     // var getPasswordSpecial = window.prompt("Would you like to include special characters? Type Y for YES and N for NO.");
-//     // console.log(getPasswordSpecial);
-//   };
-// };
-
-
-
-
-
-
-
-// // Assignment code here
-// var characterSet = [  
-//   ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-//   ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-//   ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
-//   [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "backslash", "]", "^", "_", "`", "{", "|", "}", "~"]
-// ];
-
-// var password = {
-//   length: getPasswordLength()
-//   // lowerCase: getPasswordLower,
-//   // upperCase: getPasswordUpper,
-//   // number: getPasswordNumber, 
-//   // specialCharacter: getPasswordSpecial
-// };
+*/
